806457d6489ef9cf8816d95e2cec86e629a278b0
Priyanshu Ghosh <priyanshughosh2001.pg@gmail.com>
2025-04-20 19:05:47 +0530

    Adding source code for user authentication

--- backend/index.js
+++ backend/index.js
@@ -3,11 +3,19 @@ const dotenv = require("dotenv");
 dotenv.config();
 const cors = require("cors");
 const connectToDb = require("./config/db");
+
+const authRoutes = require('./routes/authRoutes')
+// const profileRoutes =require('./routes/profileRoutes')
+
 const app = express();
 const port = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(express.json());
+
+app.use('/api/auth',authRoutes)
+// app.use('/api/profile',profileRoutes)
+
 
 //connect to db and start server
 connectToDb();
